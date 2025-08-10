f727ebfaeea76b737f608b08a2dc6ff018154c20
Shek Armanul Haque <shekarmanulhoque@gmail.com>
2025-08-11 03:24:40 +0600

    update

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -1,23 +1,21 @@
 #include<iostream>
 using namespace std;
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
+void BubbleSort(int arr[],int n){
+    for(int i=0;i<n;i++){
 
-        // Move elements of arr[0..i-1], that are greater than key,
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j+1]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
         }
-        arr[j + 1] = key;
     }
 }
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
-    insertionSort(arr, n);
+    BubbleSort(arr, n);
     cout << "Sorted array: \n";
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -1,25 +1,26 @@
 #include<iostream>
 using namespace std;
-void insertionSort(int arr[], int n) {
-    for (int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        // Move elements of arr[0..i-1], that are greater than key,
-        // to one position ahead of their current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
+void insertionsort(int arr[],int n){
+    int temp;
+    for(int i=1;i<n;i++){
+        for(int j=i;j>0;j--){
+            if(arr[j]<arr[j-1]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
         }
-        arr[j + 1] = key;
+        else{
+            break; // No need to continue if the current element is in the correct position
+        }
     }
 }
-int main() {
-    int arr[] = {12, 11, 13, 5, 6};
+}
+int main(){
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
-    insertionSort(arr, n);
+    insertionsort(arr, n);
     cout << "Sorted array: \n";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    for (int i = 0; i < n; i++){
+        cout << arr[i] << " ";}
     return 0;
 }
